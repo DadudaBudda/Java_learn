3f65671d2677f742f636df2cc66a9f592e002e6a
mschuchin <mschuchin77@gmail.com>
2020-06-17 16:53:00 +0300

    Exercise7

--- src/Exercise7.java
+++ src/Exercise7.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Exercise7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int X = 10;
+        final int Y = 20;
+        final int Z = 30;
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+        if (number == X || number == Y || number == Z)
+            System.out.println("Данное значение имеется в константах");
+        else
+            System.out.println("Такой константы нет!");
+    }
+
 }

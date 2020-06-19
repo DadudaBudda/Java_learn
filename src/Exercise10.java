b5683761abca1886f42c27918f05933b8bf8d4d5
mschuchin <mschuchin77@gmail.com>
2020-06-19 13:22:28 +0300

    исправил 10 задание: Матрица полностью заполняется пользователем Добавил вывод матрицы

--- src/Exercise10.java
+++ src/Exercise10.java
@@ -5,13 +5,23 @@ public class Exercise10 {
         System.out.println("Введите размер матрицы: ");
         int[][] matrix = new int[new Scanner(System.in).nextInt()][new Scanner(System.in).nextInt()];
         for (int x = 0; x < matrix.length; ++x) {
-            for (int i = 0; i < matrix.length; ++i) {
+            for (int i = 0; i < matrix[x].length; ++i) {
                 System.out.print("Введите элемент матрицы: ");
                 matrix[x][i] = new Scanner(System.in).nextInt();
             }
         }
+        System.out.println("Вывод первой строки матрицы:");
         for(Integer i : matrix[0]) {
-            System.out.println(i * 3);
+            System.out.print("\t" + i * 3);
+        }
+        System.out.println();
+        System.out.println("Матрица:");
+
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                System.out.print("\t" + anInt);
+            }
+            System.out.println();
         }
     }
 }

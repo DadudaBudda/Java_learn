6a87f322511daa342b39d3a34a44e5becfadd45d
mschuchin <mschuchin77@gmail.com>
2020-06-22 15:37:52 +0300

    доработка Start

--- src/Start.java
+++ src/Start.java
@@ -6,22 +6,62 @@ public class Start {
         var exercise = 1;
         while (exercise > 0){
 
-            System.out.print("Введите номер задачи или 0 для выхода: ");
+            System.out.print("\n Введите номер задачи или 0 для выхода: ");
             exercise = scanner.nextInt();
-
-
+            String msg = "Задача №" + exercise + "\n";
             switch (exercise) {
-                case 4 -> Exercise4.main(args);
-                case 5 -> Exercise5.main(args);
-                case 6 -> Exercise6.main(args);
-                case 7 -> Exercise7.main(args);
-                case 8 -> Exercise8.main(args);
-                case 9 -> Exercise9.main(args);
-                case 10 -> Exercise10.main(args);
-                case 11 -> Exercise11.main(args);
-                case 12 -> Exercise12.main(args);
-                case 13 -> Exercise13.main(args);
-                case 14 -> Exercise14.main(args);
+                case 2 -> {
+                    System.out.print(msg);
+                    HelloWorld.main(args);
+                }
+                case 3 -> {
+                    System.out.print(msg);
+                    Multiplplicationtable.main(args);
+                }
+                case 4 -> {
+                    System.out.print(msg);
+                    Exercise4.main(args);
+                }
+                case 5 -> {
+                    System.out.print(msg);
+                    Exercise5.main(args);
+                }
+                case 6 -> {
+                    System.out.print(msg);
+                    Exercise6.main(args);
+                }
+                case 7 -> {
+                    System.out.print(msg);
+                    Exercise7.main(args);
+                }
+                case 8 -> {
+                    System.out.print(msg);
+                    Exercise8.main(args);
+                }
+                case 9 -> {
+                    System.out.print(msg);
+                    Exercise9.main(args);
+                }
+                case 10 -> {
+                    System.out.print(msg);
+                    Exercise10.main(args);
+                }
+                case 11 -> {
+                    System.out.print(msg);
+                    Exercise11.main(args);
+                }
+                case 12 -> {
+                    System.out.print(msg);
+                    Exercise12.main(args);
+                }
+                case 13 -> {
+                    System.out.print(msg);
+                    Exercise13.main(args);
+                }
+                case 14 -> {
+                    System.out.print(msg);
+                    Exercise14.main(args);
+                }
                 default -> System.out.print("Такого задания нет");
             }
         }
